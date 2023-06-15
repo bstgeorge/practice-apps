3193c7ae8427b8b3dde1d144795bff0389675de6
Brett St. George <brett.stgeorge@gmail.com>
2023-06-15 12:35:16 -0400

    basic client side UI build

---
+++ 1-glossary/client/src/components/Words.jsx
@@ -0,0 +1,10 @@
+export default function Words() {
+  return (
+    <dl>
+      <dt>Word:</dt>
+      <dd>Definition</dd>
+      <button type="submit">Edit</button>
+      <button type="submit">Delete</button>
+    </dl>
+  )
+}
--- 1-glossary/client/src/index.jsx
+++ 1-glossary/client/src/index.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import { render } from "react-dom";
+//const path = require('path');
+import Words from "./components/Words.jsx";
 
 render(
   <div>
-    <p>Hello, World!</p>
+    <h1>Glossary</h1>
+    <form>
+      <p>
+        <label for="word">
+          Word:<br></br><input type="text" id="word" name="word"></input>
+        </label>
+        <br></br>
+        <label for="defintion">
+          Definition:<br></br><input type="text" id="definition" name="definition"></input>
+        </label>
+      </p>
+    </form>
+    <button type="submit">Submit</button>
+    <br></br>
+    <h3>Current Words & Definitions</h3>
+    <Words />
   </div>,
   document.getElementById("root")
 );
